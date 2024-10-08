d6ef9db275ccff6f3042dec46f7ba62f00930012
Harash <harash.poriya@gmail.com>
2024-10-08 21:30:47 +0530

    create route and controllers

---
+++ Backends/routes/user.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const User = require("../model/user");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { config } = require("dotenv");
+config();
+
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body; 
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
+    const createUser = await User.create({
+      username,
+      email,
+      password: hashPassword,
+    });
+    const token = jwt.sign(
+      { user: createUser._id },
+      process.env.secretKey,
+      { expiresIn: "2d" }
+    );
+    return res
+      .status(200)
+      .json({ message: "Create Account Successful", createUser, token });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Account not found" });
+    }
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.status(401).json({ message: "Password does not match" });
+    }
+    const token = jwt.sign(
+      { user: user._id },
+      process.env.secretKey,
+      { expiresIn: "2d" }
+    );
+    return res
+      .status(200)
+      .json({ message: "Signin Successful", user, token });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
