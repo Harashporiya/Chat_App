64e3f9e50261cbfc48630b2ddddd86550bbb6eb2
Harash <Harashporiya>
2024-10-08 21:30:47 +0530

    create route and controllers

--- Backends/index.js
+++ Backends/index.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
 const PORT = 6006;
+const userRouter = require("./routes/user")
 
-mongoose.connect("mongodb://localhost:27017/chat_App")
+mongoose.connect("mongodb://localhost:27017/chat_App",{})
 .then(()=>{
     console.log("Mongodb Connect")
 })
 app.get("/", (req,res)=>{
     return res.send("Chat App")
 })
-
+app.use(express.json())
+app.use("/api", userRouter);
 
 app.listen(PORT,()=>console.log(`Server Started At PORT: ${PORT}`))
