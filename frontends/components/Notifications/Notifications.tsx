66da65d7b16822dfb080e1531ca71d4949835bc3
Harash <harash.poriya@gmail.com>
2024-10-09 21:27:40 +0530

    created at users and settings and notifications files

---
+++ frontends/components/Notifications/Notifications.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text, ScrollView} from "react-native"
+import FooterPage from "../footer";
+
+const NotificationsPage = ()=>{
+    return (
+        <>
+        <ScrollView>
+            <Text>Notifications</Text>
+        </ScrollView>
+        <FooterPage/>
+        </>
+    )
+}
+
+export default NotificationsPage;
---
+++ frontends/components/Users/Users.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text, ScrollView} from "react-native"
+import FooterPage from "../footer";
+
+const UsersPage = ()=>{
+    return (
+        <>
+        <ScrollView>
+            <Text>UsersPage</Text>
+        </ScrollView>
+        <FooterPage/>
+        </>
+    )
+}
+
+export default UsersPage;
