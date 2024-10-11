98ea5acf03bd52a414ce6e7343da38782728d1a5
Harash <Harashporiya>
2024-10-11 20:58:46 +0530

    Get request user by user id successfull

--- Backends/routes/user.js
+++ Backends/routes/user.js
@@ -54,11 +54,26 @@ router.post("/signin", async (req, res) => {
 
 router.get("/all/users", async(req,res)=>{
   try {
-    const allUser = await User.findOne({})
+    const allUser = await User.find({})
     if(!allUser){
       return res.status(404).json({message:"Users not found"})
     }
     return res.status(200).json({message:"All user fetch", allUser})
+  } catch (error) {
+    return res.status(500).json({message:"Failed the fetch data"})
+  }
+})
+
+router.get('/user/:id', async(req,res)=>{
+  const params =  req.params;
+  const id= params.id;
+  console.log(id)
+  try {
+    const UserIdBy = await User.findById({_id:id})
+    if(!UserIdBy){
+      return res.status(404).json({message:"Users not found"})
+    }
+    return res.status(200).json({message:"User fetch", UserIdBy})
   } catch (error) {
     return res.status(500).json({message:"Failed the fetch data"})
   }
