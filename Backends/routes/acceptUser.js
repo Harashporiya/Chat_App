f20e50c208716f78a51bcd3df349de57784fdce5
Harash <Harashporiya>
2024-10-12 16:42:53 +0530

    create model and controllers for user accepts request for friends request is done

---
+++ Backends/routes/acceptUser.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const AcceptUser = require("../model/acceptUser");
+const router = express.Router();
+
+router.post("/userId", async (req, res) => {
+    const { loginUserId, username, sentFriendId, sentFriendUsername } = req.body;
+    
+    console.log(req.body);
+    
+    try {
+        const userNew = await AcceptUser.create({ loginUserId, username, sentFriendId, sentFriendUsername });
+        console.log(userNew);
+        return res.status(200).json({ message: "User id stored successfully", userNew });
+    } catch (error) {
+        console.error("Error storing user id:", error);
+        return res.status(500).json({ error: 'Failed to store the user id', details: error.message });
+    }
+});
+
+router.get("/sent", async (req, res) => {
+    try {
+        const userdata = await AcceptUser.find({});
+        if (userdata.length === 0) {
+            return res.status(404).json({ message: "No user data found" });
+        }
+        return res.status(200).json({ message: "All request fetch data", userdata });
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+        return res.status(500).json({ message: "Internal server error", details: error.message });
+    }
+});
+
+module.exports = router;
