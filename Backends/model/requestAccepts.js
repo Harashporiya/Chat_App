68d848ead78b6ee41139c0897efffbd5ecd5b1a7
Harash <harash.poriya@gmail.com>
2024-10-13 11:33:25 +0530

    get method and model keyword add

--- Backends/model/requestAccepts.js
+++ Backends/model/requestAccepts.js
@@ -13,6 +13,14 @@ const requestSchema = new Schema({
         type: String,
         default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s",
     },
+    loginUsername:{
+        type:String,
+        required:true,
+    },
+    loginUserId:{
+        type:String,
+        required:true,
+    }
 }, {timestamps:true})
 
 const RequestAccepts = model("RequestAccepts", requestSchema);
--- Backends/routes/requestAccepts.js
+++ Backends/routes/requestAccepts.js
@@ -3,12 +3,22 @@ const RequestAccepts = require("../model/requestAccepts");
 const router = express.Router();
 
 router.post("/accepts", async(req,res)=>{
-    const {username, acceptUserId} = req.body;
+    const {username, acceptUserId, loginUsername, loginUserId} = req.body;
     try {
         const acceptsUser = await RequestAccepts.create({
-            username,acceptUserId
+            username,acceptUserId, loginUsername, loginUserId
         })
         return res.status(200).json({message:"Accept user data store successfull", acceptsUser})
+    } catch (error) {
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+})
+
+
+router.get("/all/accepts", async(req,res)=>{
+    try {
+        const allAcceptsUser = await RequestAccepts.find();
+        return res.status(200).json({message:"Fetch the data all, accepts friends", allAcceptsUser})
     } catch (error) {
         return res.status(500).json({message:"Internal Server Error"});
     }
