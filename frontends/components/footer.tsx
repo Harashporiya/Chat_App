e960ad314f12435472438c04d54d4dbec8b41563
Harash <Harashporiya>
2024-10-13 21:35:53 +0530

    Notification count update and correct the conditions

--- frontends/components/footer.tsx
+++ frontends/components/footer.tsx
@@ -5,27 +5,74 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { RouterType } from "./Navigation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { BACKEND_URL } from "../API_BACKENDS/Backend_API";
+
+
+interface User {
+  createdAt: string;
+  loginUserId: string;
+  sentFriendId: string | null;
+  _id: string;
+  username: string;
+  profileImage: string;
+}
+
+interface UserData {
+  message: string;
+  userdata: User[];
+}
 
 const FooterPage = () => {
   const [selectedIcon, setSelectedIcon] = useState<string | null>(null);
   const [notificationCount, setNotificationCount] = useState<number | null>(null);
   const navigation = useNavigation<NavigationProp<RouterType>>();
 
-  
-  useEffect(() => {
-    const fetchNotificationCount = async () => {
-      const count = await AsyncStorage.getItem("FriendsCount");
-     
-      setNotificationCount(count ? parseInt(count) : 0);
-      // console.log("Notification Count:", count);
-    };
 
-    fetchNotificationCount();
-  }, []); 
+  // useEffect(() => {
+  //   const fetchNotificationCount = async () => {
+  //     const count = await AsyncStorage.getItem("FriendsCount");
+  //     const userId = await AsyncStorage.getItem("UserId")
+
+  // setNotificationCount(count ? parseInt(count) : 0);
+  //     // console.log("Notification Count:", count);
+  //   };
+
+  //   fetchNotificationCount();
+  // }, []); 
 
   const handleShow = (iconName: string) => {
     setSelectedIcon(prevIcon => (prevIcon === iconName ? null : iconName));
   };
+
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsersData = async () => {
+      const loggedInUserId = await AsyncStorage.getItem("UserId");
+      setUserId(loggedInUserId);
+
+      try {
+        const res = await axios.get(`${BACKEND_URL}/api/friend/sent`);
+        const filteredRequests = res.data.userdata.filter((request: User) => request.sentFriendId === loggedInUserId);
+        setUserData({ ...res.data, userdata: filteredRequests });
+        // setNotificationCount((filteredRequests).file(user=>))
+        const filteredRequestsCount = res.data.userdata.filter((request: User) => request.sentFriendId === loggedInUserId)
+        const count = filteredRequestsCount.length
+        setNotificationCount(count ? parseInt(count) : 0);
+        // console.log(res.data)
+
+        //  const count = filteredRequests.length;
+        //  await AsyncStorage.setItem("FriendsCount",count.toString())
+        //  console.log(count)
+      } catch (error) {
+        console.log("Failed to fetch users data: ", error);
+      }
+    };
+
+    fetchUsersData();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -43,9 +90,13 @@ const FooterPage = () => {
       </View>
       <View style={styles.item}>
         <View style={styles.notificationContainer}>
-          {notificationCount !== null && notificationCount > 0 && (
+          {notificationCount !== null && notificationCount > 0 ? (
             <View style={styles.notificationCount}>
               <Text style={styles.countText}>{notificationCount}</Text>
+            </View>
+          ) : (
+            <View style={styles.notificationCount}>
+              <Text style={styles.countText}>0</Text>
             </View>
           )}
           <Ionicons
@@ -107,8 +158,8 @@ const styles = StyleSheet.create({
   },
   notificationCount: {
     position: 'absolute',
-    top: -10, 
-    right: -10, 
+    top: -10,
+    right: -10,
     backgroundColor: 'red',
     borderRadius: 9,
     paddingHorizontal: 6,
