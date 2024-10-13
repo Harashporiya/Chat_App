c626c6464a860733fe55a88650d185018fb30d62
Harash <Harashporiya>
2024-10-13 09:03:31 +0530

    Request accept user model / Schema create

---
+++ Backends/model/requestAccepts.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const requestSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    acceptUserId:{
+        type:String,
+        required:true,
+    },
+    profileImage: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s",
+    },
+}, {timestamps:true})
+
+const RequestAccepts = model("RequestAccepts", requestSchema);
+module.exports = RequestAccepts;
