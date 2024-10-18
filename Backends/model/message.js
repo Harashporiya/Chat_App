001fa24d25f3e2979db39c2178cc9a792db7001f
Harash <harash.poriya@gmail.com>
2024-10-18 14:06:25 +0530

    add

--- Backends/model/message.js
+++ Backends/model/message.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   senderId: { type: String, required: true },
+  receiverId: { type: String, required: true },
   message: { type: String, required: true },
   roomId: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
