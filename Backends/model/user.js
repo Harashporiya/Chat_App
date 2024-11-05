9507b61f01e25d6bca3837e973a6099ce058b3eb
Harash <harash.poriya@gmail.com>
2024-11-05 15:25:42 +0530

    Add profile image and background Image

--- Backends/model/user.js
+++ Backends/model/user.js
@@ -1,24 +1,28 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
+    username: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true
     },
-    profileImage:{
-        type:String,
-        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s",
+    profileImage: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s"
+    },
+    backgroundImage: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2029&auto=format&fit=crop"
     }
-},{timestamps:true});
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
