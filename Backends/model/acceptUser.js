9acb5e72f372e1f131645d763539203b7b41d898
Harash <harash.poriya@gmail.com>
2024-11-12 20:35:19 +0530

    add profile image

--- Backends/model/acceptUser.js
+++ Backends/model/acceptUser.js
@@ -6,7 +6,7 @@ const acceptUser = new mongoose.Schema({
     },
     profileImage: {
         type: String,
-        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s",
+        
     },
     username: {
         type: String,
