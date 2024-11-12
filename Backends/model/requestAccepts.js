1f49bba27c080b7e269aafa8e5a9b1c7ee25c0d3
Harash <Harashporiya>
2024-11-12 20:36:00 +0530

    add profile image

--- Backends/model/requestAccepts.js
+++ Backends/model/requestAccepts.js
@@ -10,8 +10,7 @@ const requestSchema = new Schema({
         required:true,
     },
     profileImage: {
-        type: String,
-        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s",
+        type:String
     },
     loginUsername:{
         type:String,
